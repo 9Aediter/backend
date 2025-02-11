18be3e765dd3e0b8651736a440fd91552572244c
9Aediter <mr.anonsuphatphon@gmail.com>
2025-02-11 17:03:20 +0700

    Update backend submodule

---
+++ src/app.module.ts
@@ -0,0 +1,29 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+
+@Module({
+  imports: [
+    // เพิ่มไว้สำหรับการอ่าน config ผ่าน .env
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    
+    // ใช้ค่าจาก .env แทนฮาร์ดโค้ด
+    MongooseModule.forRoot(`mongodb://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}`, {
+      user: process.env.MONGO_USER,
+      pass: process.env.MONGO_PASS,
+      dbName: process.env.MONGO_DB,
+    }),
+    AuthModule,
+    UserModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
